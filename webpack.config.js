06854048a75688446c99451fdb4a32ae3d895eed
galaxxxy <2511030361@qq.com>
2020-03-30 14:23:18 +0800

    webpack配置css单独分离打包

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HTMLPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -9,7 +10,7 @@ const config = {
   target: 'web',
   entry: path.join(__dirname, './src/index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist'),
   },
   module: {
@@ -19,29 +20,8 @@ const config = {
         loader: 'vue-loader',
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
         test: /\.jsx$/,
         loader: 'babel-loader',
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          'stylus-loader',
-        ],
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -69,6 +49,20 @@ const config = {
 };
 
 if (isDev) {
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+        },
+      },
+      'stylus-loader',
+    ],
+  });
   config.devtool = '#cheap-module-eval-source-map';
   config.devServer = {
     port: 8000,
@@ -82,6 +76,27 @@ if (isDev) {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   )
+} else {
+  config.output.filename = '[name].[chunkhash:8].js';
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+        'stylus-loader',
+      ],
+    }),
+  });
+  config.plugins.push(
+    new ExtractPlugin('styles.[md5:contentHash:hex:8].css')
+  );
 }
 
 module.exports = config;
