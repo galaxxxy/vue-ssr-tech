211dfc9a073d69d127b7836fbbd200d98be5021a
galaxxxy <2511030361@qq.com>
2020-04-01 20:10:58 +0800

    css module配置

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,7 +2,10 @@ module.exports = (isDev) => {
   return {
     preserveWhitespace: true,
     extractCSS: !isDev,
-    cssModules: {},
+    cssModules: {
+      localIndentName: '[path]-[name]-[hash:base64:5]',
+      camelCase: true,
+    },
     // hotReload: false, 根据环境变量生成
   };
 };
