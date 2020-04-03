5bd069b4d1ee182165c7e7321a6e0191757666ad
galaxxxy <2511030361@qq.com>
2020-04-03 18:56:43 +0800

    vue实例

--- practice/index.js
+++ practice/index.js
@@ -1,9 +1,1 @@
-import Vue from 'vue'
-
-const div = document.createElement('div')
-document.body.appendChild(div)
-
-new Vue({
-  el: '#root',
-  template: '<div>hello vue</div>'
-})
+import './instance'
