a35314d93aefaa0806fd8b77e6a31e0bbce47dd0
galaxxxy <2511030361@qq.com>
2020-04-10 19:55:43 +0800

    actions和mutations

--- client/app.vue
+++ client/app.vue
@@ -20,16 +20,12 @@
 <script>
 import Header from './views/layout/header.vue'
 import Footer from './views/layout/footer.jsx'
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
 
 export default {
   components: {
     Header,
     Footer
-  },
-  data () {
-    return {
-    }
   },
   computed: {
     ...mapState({
@@ -39,12 +35,24 @@ export default {
       fullName: 'fullName'
     })
   },
+  methods: {
+    ...mapActions(['updateCountAsync']),
+    ...mapMutations(['updateCount'])
+  },
   mounted () {
     console.log(this.$store)
-    let i = 1
-    setInterval(() => {
-      this.$store.commit('updateCount', i++)
-    }, 1000)
+    // let i = 1
+    // setInterval(() => {
+    //   this.updateCount(i++)
+    // }, 1000)
+    // this.$store.dispatch('updateCountAsync', {
+    //   num: 5,
+    //   time: 2000
+    // })
+    this.updateCountAsync({
+      num: 5,
+      time: 2000
+    })
   }
 }
 </script>
